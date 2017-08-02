e138d3908ee701ee100c820da9b73335f37e9215
Bjarne <bjakke112@gmail.com>
2017-08-02 15:14:38 +0200

    changed from potentiometer to buttons for a more realistic input for using to RotaryEncoders

--- Program.cs
+++ Program.cs
@@ -11,15 +11,13 @@ namespace Volumecontrol
         public void setVolume(int value){
             System.Diagnostics.Process volumeProcess = new System.Diagnostics.Process();
             volumeProcess.StartInfo.FileName = "nircmd.exe";
-            volumeProcess.StartInfo.Arguments = "setsysvolume " + 65535*(value/1023);
+            volumeProcess.StartInfo.Arguments = "setsysvolume " + (65535*(value/1023));
             volumeProcess.Start();
             volumeProcess.WaitForExit();
-            volumeProcess.Close();
 
         }
-        static void Main(string[] args)
-        {
-            Program volume = new Program();
+
+        public string getInput(){
             try{
                 
                 //starting and opening serial port com5
@@ -27,29 +25,41 @@ namespace Volumecontrol
                 System.Console.WriteLine("Connected to COM5");
                 serialport.BaudRate = 9600;
                 serialport.Open();
+                return serialport.ReadLine();
+                serialport.Close();
+            }catch(Exception e){
+                Console.WriteLine(e);
+                return "0";
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Program volume = new Program();
+            
                 
                 
                 string read = "1023";
-                int oldVolume = 0;
+                int oldVolume = 1023;
                 int newVolume;
 
                 while(!read.Equals("0")){
-                    
-                    read = serialport.ReadLine();
-                    
-                    Int32.TryParse(read, out newVolume);
-                    
-                    if (!newVolume.Equals(oldVolume))
+                    read = volume.getInput();                    
+                    if (Int32.TryParse(read,out newVolume))
+                    {       
+                        if (newVolume != oldVolume)
+                        {
+                            oldVolume = newVolume;
+                            volume.setVolume(oldVolume);
+                            Console.WriteLine("setting volume: " + read);
+                        }
+                    }else
                     {
-                        volume.setVolume(newVolume);
-                        oldVolume = newVolume;
-                        Console.WriteLine("setting volume: " + read);
+                        System.Console.WriteLine("Could not parse to int");
                     }
                     
                 }
-            }catch(Exception e){
-                Console.WriteLine(e);
-            }
             
         }
     }
