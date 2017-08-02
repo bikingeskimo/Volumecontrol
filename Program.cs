a6635b61db24b686f8538b65708e123ff694ae2c
Bjarne <bjakke112@gmail.com>
2017-08-02 17:28:31 +0200

    changed the way the program connects to serial for a faster and more efficient way of using inputs
    also methods

--- Program.cs
+++ Program.cs
@@ -6,27 +6,47 @@ namespace Volumecontrol
 {
     class Program
     {
+        int maxVolume = 65535;
 
         
-        public void setVolume(int value){
+        //adjusting volume up or down by 65535(max)/number of buttonpresses needed to go from 0-100
+        public void setVolume(string value,int steps){
             System.Diagnostics.Process volumeProcess = new System.Diagnostics.Process();
             volumeProcess.StartInfo.FileName = "nircmd.exe";
-            volumeProcess.StartInfo.Arguments = "setsysvolume " + (65535*(value/1023));
-            volumeProcess.Start();
-            volumeProcess.WaitForExit();
+            if(value.Equals("1")){
+                volumeProcess.StartInfo.Arguments = ("changesysvolume " + maxVolume/steps).ToString();
+                System.Console.WriteLine("Volumchange " + maxVolume/steps);
+                volumeProcess.Start();
+                volumeProcess.WaitForExit();
+                                
+            }
+            if(value.Equals("-1")){
+                volumeProcess.StartInfo.Arguments = ("changesysvolume " + -maxVolume/steps).ToString();
+                System.Console.WriteLine("Volumchange " + -maxVolume/steps);
+                volumeProcess.Start();
+                volumeProcess.WaitForExit();
+            }
 
         }
 
-        public string getInput(){
+        public SerialPort connectSerial(string portName){
+                //starting and opening serial port
             try{
-                
-                //starting and opening serial port com5
-                SerialPort serialport = new SerialPort("COM5");
-                System.Console.WriteLine("Connected to COM5");
+                SerialPort serialport = new SerialPort(portName);
+                System.Console.WriteLine("Connected to: " + portName);
                 serialport.BaudRate = 9600;
                 serialport.Open();
-                return serialport.ReadLine();
-                serialport.Close();
+                return serialport;
+            }catch(Exception e){
+                System.Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public string getInput(SerialPort serialport){
+            try{
+                string output = serialport.ReadLine();
+                return output;
             }catch(Exception e){
                 Console.WriteLine(e);
                 return "0";
@@ -37,30 +57,10 @@ namespace Volumecontrol
         static void Main(string[] args)
         {
             Program volume = new Program();
-            
-                
-                
-                string read = "1023";
-                int oldVolume = 1023;
-                int newVolume;
-
-                while(!read.Equals("0")){
-                    read = volume.getInput();                    
-                    if (Int32.TryParse(read,out newVolume))
-                    {       
-                        if (newVolume != oldVolume)
-                        {
-                            oldVolume = newVolume;
-                            volume.setVolume(oldVolume);
-                            Console.WriteLine("setting volume: " + read);
-                        }
-                    }else
-                    {
-                        System.Console.WriteLine("Could not parse to int");
-                    }
-                    
-                }
-            
+            SerialPort sp = volume.connectSerial("COM5");
+            while(true){
+                volume.setVolume(volume.getInput(sp),20);
+            }
         }
     }
 }
